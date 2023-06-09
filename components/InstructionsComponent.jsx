74bd3e37d421d295db59f47ac26d621453981f50
Booker-CU <booker3@gmail.com>
2023-06-09 10:53:54 -0400

    inital commit

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -3,11 +3,14 @@ import Router, { useRouter } from "next/router";
 import { useSigner, useNetwork, useBalance, useContract } from "wagmi"
 import { ethers } from "ethers";
 import { useState, useEffect } from 'react';
-import tokenABI from "./assets/MyERC20Token.json";
+import tokenABI from "./assets/MyERC20Votes.json";
+import tokenizedBallotABI from "./assets/TokenizedBallot.json"
 
 
 
 const tokenAddress = "0x0936Be36ACd102CC7AF17Df760ca390f69891a17";
+const tokenizedBallotAddress = "0xfbFa68808D69E306d4AAF8F8647EfDA45F815C7F";
+
 const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 
 const provider = new ethers.providers.AlchemyProvider(
@@ -15,20 +18,21 @@ const provider = new ethers.providers.AlchemyProvider(
 	apiKey
 )
 
-const tokenContract = new ethers.Contract(tokenAddress,tokenABI.abi, signer)
+const tokenContract = new ethers.Contract(tokenAddress,tokenABI.abi,provider);
+const tokenizedBallot = new ethers.Contract(tokenizedBallotAddress,tokenizedBallotABI.abi, provider)
 
 
 
 export default function InstructionsComponent() {
 	const router = useRouter();
 
-	const {data: signer, isError, isLoading } = useSigner();
+
 
 	return (
 		<div className={styles.container}>
 			<header className={styles.header_container}>
 				<h1>
-					mydapp
+					VotingDapp
 
 				</h1>
 
@@ -47,27 +51,29 @@ export default function InstructionsComponent() {
 
 function Pageblock() {
 	return(
-		<div>
+		<div className={styles.container}>
 			<WalletInfo/>
 		</div>
 	)
 }
+
 function WalletInfo() {
 
 	const {data: signer, isError, isLoading } = useSigner();
 	const {chain, chains} = useNetwork();
 
 	if (signer) return(
-		<divv>
-			<p>{signer._address}</p>
-			<p>{chain.name}</p>
-			<button onClick={() => signMessage(signer, "sign my message")}>Sign</button>
-			<WalletBalance/>
-			<ApiInfo/>
+		<div className={styles.buttons_container}>
+			{/* <p>{signer._address}</p>
+			<p>{chain.name}</p> */}
+			{/* <button onClick={() => signMessage(signer, "sign my message")}>Sign</button> */}
+			{/* <WalletBalance/> */}
 			<RequestTokens/>
+			<DelegateTokens/>
+			<Vote/>
+			{/* <WinningProposal/> */}
 
-
-		</divv>
+		</div>
 	)
 	else if (isLoading )return(
 		<div>
@@ -136,18 +142,21 @@ function RequestTokens() {
 	const [isLoading, setLoading] = useState(false);
 
 	if (txData) return (
-		<div>
+		<div >
 			<p>Transaction Complete!</p>
-			<a href={"https://mumbai.polygonscan.com/tx/" + txData.hash} target="_blank">{txData.hash}</a>
-			<button onClick={() => delegateTokens }> Delegate</button>
+			<div className={styles.button_textTx_wrap}>
+			<a  href={"https://mumbai.polygonscan.com/tx/" + txData.hash} target="_blank">{txData.hash}</a>
+			</div>
 		</div>
 	)
+
 	if(isLoading) return <p>Requesting Tokens To Be Minted </p>
 	return (
 		<div>
-			<button onClick={() => requestTokens(signer,"signature", setLoading, setTxData)}>Tokens Please </button>
+			<button className={styles.button} onClick={() => requestTokens(signer,"signature", setLoading, setTxData)}>Tokens Please </button>
 		</div>
 	)
+
 }
 
 function requestTokens(signer, signature, setLoading, setTxData) {
@@ -157,7 +166,7 @@ function requestTokens(signer, signature, setLoading, setTxData) {
 		headers: {"Content-Type" : "application/json"},
 		body: JSON.stringify({ address: signer._address, signature: signature })
 	};
-	fetch("http://localhost:3006/request-tokens", requestOptions)
+	fetch("http://localhost:3003/request-tokens", requestOptions)
 	.then(response => response.json())
 	.then((data) => {
 		setTxData(data);
@@ -165,11 +174,37 @@ function requestTokens(signer, signature, setLoading, setTxData) {
 	})
 }
 
-function delegateTokens(address) {
+function DelegateTokens() {
 	const {data: signer} = useSigner();
-	tokenContract.connect(signer).mint()
+
+	return (
+		<div>
+			<p><button className={styles.button} onClick={() => tokenContract.connect(signer).delegate(signer._address) }> Delegate</button></p>
+		</div>
+	)
 }
 
-function vote() {
+function Vote() {
+	const {data: signer} = useSigner();
 
+	return (
+		<div >
+			<button className={styles.button} onClick={() => tokenizedBallot.connect(signer).vote(0,ethers.utils.parseUnits("1")) }> Vote Proposal 1 </button>
+			<button className={styles.button} onClick={() => tokenizedBallot.connect(signer).vote(1,ethers.utils.parseUnits("1")) }> Vote Proposal 2 </button>
+			<button className={styles.button} onClick={() => tokenizedBallot.connect(signer).vote(2,ethers.utils.parseUnits("1")) }> Vote Proposal 3 </button>
+		</div>
+	)
 }
+
+/* function WinningProposal() {
+
+	const winner =  tokenizedBallot.winnerName()
+
+	return <div>The winner is {winner.name}</div>
+} */
+
+
+
+
+
+
